ee4321f923295ac2acd5e8f0a77e2cfd8d07ecb7
Joshua T Kalis <kalisjoshua@gmail.com>
2015-04-24 10:26:14 -0400

    add rule for base URI

---
+++ test/rule-base_uri.js
@@ -0,0 +1,26 @@
+var assert = require('assert'),
+    rule = require('../src/rules/base_uri.js');
+
+describe('Rules - Base URI', function () {
+  it('should be an function', function () {
+    assert.equal('function', typeof rule);
+  });
+
+  it('should provide a level', function () {
+    assert(rule.level);
+  });
+
+  it('should provide a message', function () {
+    assert(rule.message);
+  });
+
+  it('should provide a section', function () {
+    assert(rule.section);
+    assert(/root|resource|method/i.test(rule.section));
+  });
+
+  it('should return true for a valid context', function () {
+    assert.strictEqual(true, rule({baseUri: 'http://example.com'}));
+    assert.strictEqual(false, rule({}));
+  });
+});

8a9b914157cd242fc6acc88d8742a18eded5c503
Joshua T Kalis <kalisjoshua@gmail.com>
2015-04-24 17:06:04 -0400

    more use of typeOf() in rules

--- src/rules/base_uri.js
+++ src/rules/base_uri.js
@@ -1,21 +1,21 @@
-(function () {
+var typeOf = require('../typeOf.js');
 
-  // obj will be the context that is needed for validation
-  function rule(obj) {
+// obj will be the context that is needed for validation
+function rule(obj) {
 
-    return !!obj.baseUri;
-  }
+  return !!obj.baseUri;
+}
 
-  // log level
-  rule.level = 'error';
+// log level
+rule.level = 'error';
 
-  // message for log
-  rule.message = 'RAML file must include a baseUri.';
+// message for log
+rule.message = 'RAML file must include a baseUri.';
 
-  //section of document to check
-  rule.section = 'root';
+//section of document to check
+rule.section = 'root';
 
-  if (typeof exports === 'object' && exports) {
-    module.exports = rule;
-  }
-}());
+/* istanbul ignore else */
+if (typeof(exports, 'object')) {
+  module.exports = rule;
+}
--- src/rules/resource_desc.js
+++ src/rules/resource_desc.js
@@ -1,23 +1,21 @@
 var typeOf = require('../typeOf.js');
 
-(function () {
+// obj will be the context that is needed for validation
+function rule(obj) {
 
-  // obj will be the context that is needed for validation
-  function rule(obj) {
+  return !!obj.description;
+}
 
-    return !!obj.description;
-  }
+// log level
+rule.level = 'error';
 
-  // log level
-  rule.level = 'error';
+// message for log
+rule.message = 'Resources need to provide a description.';
 
-  // message for log
-  rule.message = 'Resources need to provide a description.';
+//section of document to check
+rule.section = 'resource';
 
-  //section of document to check
-  rule.section = 'resource';
-
-  if (typeOf(exports, 'object')) {
-    module.exports = rule;
-  }
-}());
+/* istanbul ignore else */
+if (typeOf(exports, 'object')) {
+  module.exports = rule;
+}
